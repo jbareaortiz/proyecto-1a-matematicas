fa79291f25af5eac60136f084e3380a7529d7f92
josemanuel.barea <josemanuel.barea@condenast.es>
2020-11-08 09:55:14 +0100

    Mejora en la doc. Mejora en la librería.

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
-# Operaciones matemáticas
+# Operaciones matematicas
 
-Módulo para hacer operaciones matemáticas simples.
+Modulo para realizar operaciones matematicas simples
 
-# Intalación
-...
-    npm install proyecto-1a-matematicas-jb
-...
+## Instalación
+Seguir estas instrucciones
+
+```
+npm install proyecto-1a-matematicas
+```
 
 # Uso
+Seguir estas instrucciones
 
-...
-    const m = require('proyecto-1a-matematicas-jb');
-    console.log(m.suma(2,5)); //7
-...
+```
+// Importar el modulo
+const m = require('.');
+
+// Sumas
+console.log(m.suma(1,2)); => 3
+console.log(m.suma(1,34)); => 35
+
+// Restas
+console.log(m.resta(11,24)); => -13
+console.log(m.resta(1,2)); => -1
+
+// Multiplicacion
+console.log(m.multiplicacion(1,24)); => 24
+console.log(m.multiplicacion(1,2)); => 2
+
+// Division
+console.log(m.division(1,4)); => 0.25
+console.log(m.division(1,2)); => 0.5
+```
--- index.js
+++ index.js
@@ -1,42 +1,59 @@
 module.exports = {
     /**
-     * Suma de dos números.
+     * Suma de dos numeros
      * @example
-     * n1 =1, n2=3 => resultado 4
-     * @param {*} n1 Número 1 de la suma
-     * @param {*} n2 Número 2 de la suma
+     * n1 = 1 , n2 = 3 = > resultado 4
+     * @param {*} n1 Numero uno de la suma
+     * @param {*} n2 Numero dos de la suma
      */
-    suma : function(n1, n2){
-        return n1 + n2;
+    suma: function (n1, n2) {
+        return (this.esNumero(n1, n2) ? n1 + n2: this.mensajeError());
     },
     /**
-     * Resta de dos números.
+     * Resta de dos numeros
      * @example
-     * n1 =1, n2=3 => resultado -2
-     * @param {*} n1 Número 1 de la resta
-     * @param {*} n2 Número 2 de la resta
+     * n1 = 1 , n2 = 3 = > resultado -2
+     * @param {*} n1 Numero uno de la resta
+     * @param {*} n2 Numero dos de la resta
      */
-    resta : function(n1, n2){
-        return n1 - n2;
+    resta: function (n1, n2) {
+        return (this.esNumero(n1, n2) ? n1 - n2: this.mensajeError());
     },
     /**
-     * Multiplicación de dos números.
+     * Multiplicacion de dos numeros
      * @example
-     * n1 =1, n2=3 => resultado 3
-     * @param {*} n1 Número 1 de la multiplicación
-     * @param {*} n2 Número 2 de la multiplicación
+     * n1 = 1 , n2 = 3 = > resultado 3
+     * @param {*} n1 Numero uno de la multiplicacion
+     * @param {*} n2 Numero dos de la multiplicacion
      */
-    multiplicacion : function(n1, n2){
-        return n1 * n2;
+    multiplicacion: function (n1, n2) {
+        return (this.esNumero(n1, n2) ? n1 * n2: this.mensajeError());
     },
     /**
-     * División de dos números.
+     * Division de dos numeros
      * @example
-     * n1 =1, n2=3 => resultado 0.33
-     * @param {*} n1 Número 1 de la división
-     * @param {*} n2 Número 2 de la división
+     * n1 = 1 , n2 = 3 = > resultado 0.33
+     * @param {*} n1 Numero uno de la Division
+     * @param {*} n2 Numero dos de la Division
      */
-    division : function(n1, n2){
-        return n1 / n2;
+    division: function (n1, n2) {
+        return (this.esNumero(n1, n2) ? n1 / n2: this.mensajeError());
+    },
+    /**
+     * Mensaje de error que se ejecuta cuando no tengamos valores numéricos
+     */
+    mensajeError: function() {
+        console.log('Un valor o los dos valores no son numéricos');
+    },
+    /**
+     * Comprueba que son valores numéricos
+     * @param {*} n1 
+     * @param {*} n2 
+     */
+    esNumero: function (n1, n2) {
+        if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+            return false;
+        }
+        return true;
     }
 }
